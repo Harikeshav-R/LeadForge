c333d4959a86c4d665aad8fa8d6aeac05ac67476
Harikeshav R <r.harikeshav@gmail.com>
2025-10-18 18:57:13 -0400

    **feat(leads): add Lead schema for business details**
    - Introduced a `Lead` schema to represent detailed business information, including place ID, name, address, phone number, website, rating, and more.
    - Added fields to support additional data like emails, phone numbers, social media links, and screenshots for enhanced business profiling.
    - Ensured compatibility with existing `Location` and `CapturedScreenshot` schemas for seamless integration.

---
+++ leads/app/schemas/lead.py
@@ -0,0 +1,27 @@
+from pydantic import BaseModel, Field, HttpUrl
+
+from app.schemas import CapturedScreenshot
+from app.schemas.google_maps_search import Location
+
+
+class Lead(BaseModel):
+    place_id: str = Field(..., description="ID of the place on google maps.")
+    name: str = Field(..., description="Name of the business.")
+    address: str = Field(..., description="Address of the business.")
+    phone_number: str = Field(..., description="Phone number of the business.")
+    website: HttpUrl = Field(..., description="Website of the business.")
+    rating: float = Field(..., description="Rating of the business.")
+    total_ratings: int = Field(..., description="Total number of ratings.")
+    category: str = Field(..., description="Category of the business.")
+    price_level: int = Field(..., description="Price level of the business.")
+    is_open: bool = Field(..., description="Whether the business is open.")
+    location: Location = Field(..., description="Location coordinates of the business.")
+
+    emails: list[str] = Field(default_factory=list, description="A list of unique email addresses scraped.")
+    phone_numbers: list[str] = Field(default_factory=list, description="A list of unique phone numbers scraped.")
+    social_media: list[HttpUrl] = Field(default_factory=list,
+                                        description="A list of unique social media profile links scraped.")
+
+    screenshots: list[CapturedScreenshot] = Field(default_factory=list,
+                                                  description="List of base64 encoded captured screenshots for different devices.")
+    website_review: str = Field(None, description="Business website UI/UX review from the agent.")
