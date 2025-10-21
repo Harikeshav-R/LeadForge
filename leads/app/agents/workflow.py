15425c7783b79f551d6d41d1070f3940e002f90a
Harikeshav R <r.harikeshav@gmail.com>
2025-10-20 21:04:39 -0400

    **refactor(leads): modularize workflow graph creation**
    - Refactored the workflow graph setup by introducing a `make_graph` function to encapsulate graph creation and compilation logic.
    - Updated `workflow.py` to improve readability and modularity by detaching direct calls within the main execution block.
    - Adjusted entry and finish points of the graph workflow for better clarity and flexibility.

--- leads/app/agents/workflow.py
+++ leads/app/agents/workflow.py
@@ -1,22 +1,35 @@
-from langgraph.graph import StateGraph, START, END
+from langgraph.graph import StateGraph
+from langgraph.graph.state import CompiledStateGraph
 
 from app.agents.analyze_leads import analyze_leads_node
 from app.agents.lead_generator import generate_leads_node
 from app.schemas.state import State
 
-workflow = StateGraph(State)
-workflow.add_node("generate_leads", generate_leads_node)
-workflow.add_node("analyze_leads", analyze_leads_node)
 
-workflow.add_edge(START, "generate_leads")
-workflow.add_edge("generate_leads", "analyze_leads")
-workflow.add_edge("analyze_leads", END)
+def make_graph() -> CompiledStateGraph:
+    workflow = StateGraph(State)
+    workflow.add_node("generate_leads", generate_leads_node)
+    workflow.add_node("analyze_leads", analyze_leads_node)
 
-app = workflow.compile()
+    workflow.set_entry_point("generate_leads")
+    workflow.add_edge("generate_leads", "analyze_leads")
+    workflow.set_finish_point("analyze_leads")
+
+    app = workflow.compile()
+
+    return app
+
+
+#
+#
+# async def run_workflow(state: State) -> State:
+#     app = make_graph()
+#     return await app.invoke(state)
+
 
 if __name__ == "__main__":
-    initial_state = State(city="New York", business_type="restaurant")
+    initial_state = State(city="Columbus, Ohio", business_type="restaurant")
 
-    final_state = State(**app.invoke(initial_state))
+    final_state = State(**make_graph().invoke(initial_state))
 
     print(final_state.model_dump_json(indent=2, exclude={"leads": {"__all__": {"screenshots"}}}))
