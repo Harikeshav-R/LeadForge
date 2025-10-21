e0fc2082d69f171a1b98ddf852601dabfdfcf8f1
Harikeshav R <r.harikeshav@gmail.com>
2025-10-20 21:03:02 -0400

    **feat(core): add `DEBUG` attribute to `Config` class**
    - Introduced a `DEBUG` attribute to the `Config` class for handling application debug mode based on the `DEBUG` environment variable.
    - Ensured consistency with existing configuration attributes to streamline environment setup and improve clarity.

--- leads/app/core/config.py
+++ leads/app/core/config.py
@@ -1,6 +1,8 @@
 import os
 
 class Config:
+    DEBUG: bool = os.getenv("DEBUG") == "true"
+
     DATABASE_URL: str = os.getenv("DATABASE_URL")
 
     GOOGLE_MAPS_API_KEY: str = os.getenv("GOOGLE_MAPS_API_KEY")
