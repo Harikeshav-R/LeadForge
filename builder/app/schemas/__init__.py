e89d01a6ffe9f1a83034605f287f17a1fa668c64
Harikeshav R <r.harikeshav@gmail.com>
2025-10-22 19:46:41 -0400

    feat(schemas): add schema imports for `WebsiteCoder`
    - Updated `__init__.py` in `schemas` to include `WebsiteCoderInput` and `WebsiteCoderOutput` imports.
    - Ensured seamless integration and accessibility for the newly introduced `WebsiteCoder` schemas.

--- builder/app/schemas/__init__.py
+++ builder/app/schemas/__init__.py
@@ -1,3 +1,4 @@
 from app.schemas.information_scraper import InformationScraperInput, InformationScraperOutput, PageScrapedData, Headings, Link, \
     Image
 from app.schemas.page_screenshotter import PageScreenshotterInput, PageScreenshotterOutput
+from app.schemas.website_coder import WebsiteCoderInput, WebsiteCoderOutput
