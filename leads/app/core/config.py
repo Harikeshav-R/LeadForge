6ca3c3215c6c3b2e83d0cb778feb6fa8797ec77d
Harikeshav R <r.harikeshav@gmail.com>
2025-10-22 16:36:04 -0400

    **chore(config): update database configuration to use modular PostgreSQL URL**
    - Replaced the `DATABASE_URL` with a dynamically generated `POSTGRES_URL` in `Config` for improved database connection management.
    - Added new environment variables (`POSTGRES_HOST`, `POSTGRES_PORT`, `POSTGRES_USER`, `POSTGRES_PASSWORD`, `POSTGRES_DB`) to `.env.dev.example` and `.env.prod.example`.
    - Updated `app.core.database` to utilize `Config.POSTGRES_URL` for database engine creation.
    - Ensured consistency across configuration by centralizing PostgreSQL connection parameters.

--- leads/app/core/config.py
+++ leads/app/core/config.py
@@ -1,9 +1,16 @@
 import os
 
+
 class Config:
     DEBUG: bool = os.getenv("DEBUG") == "true"
 
-    DATABASE_URL: str = os.getenv("DATABASE_URL")
+    POSTGRES_HOST: str = os.getenv("POSTGRES_HOST")
+    POSTGRES_PORT: str = os.getenv("POSTGRES_PORT")
+    POSTGRES_USER: str = os.getenv("POSTGRES_USER")
+    POSTGRES_PASSWORD: str = os.getenv("POSTGRES_PASSWORD")
+    POSTGRES_DB: str = os.getenv("POSTGRES_LEADS_DB")
+
+    POSTGRES_URL: str = f"postgresql://{POSTGRES_USER}:{POSTGRES_PASSWORD}@{POSTGRES_HOST}:{POSTGRES_PORT}/{POSTGRES_DB}"
 
     GOOGLE_MAPS_API_KEY: str = os.getenv("GOOGLE_MAPS_API_KEY")
 
