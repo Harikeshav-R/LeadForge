a26471dfa3a9490553a4c9b8cc16e0e2d15c5b93
Harikeshav R <r.harikeshav@gmail.com>
2025-10-23 10:59:12 -0400

    **refactor(deployer): simplify config imports in database module**
    - Updated `database.py` to import `Config` directly from `app.config` instead of `app.core.config`.
    - Removed unnecessary nesting by relocating `config.py` from `core` to the main `app` directory for improved structure and clarity.

--- deployer/app/database.py
+++ deployer/app/database.py
@@ -1,7 +1,7 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import declarative_base, sessionmaker
 
-from app.core.config import Config
+from app.config import Config
 
 engine = create_engine(Config.POSTGRES_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
