9662490c12ae6ab1e197e3e8c3272643ff445638
Harikeshav R <r.harikeshav@gmail.com>
2025-10-22 20:19:22 -0400

    **feat(schemas): add `State` schemas to `__init__.py` imports**
    - Updated `__init__.py` to include `State`, `StateBase`, and `StateUpdate` schemas for seamless access across modules.
    - Improved maintainability and consistency in schema imports.

--- builder/app/schemas/__init__.py
+++ builder/app/schemas/__init__.py
@@ -1,4 +1,6 @@
-from app.schemas.information_scraper import InformationScraperInput, InformationScraperOutput, PageScrapedData, Headings, Link, \
+from app.schemas.information_scraper import InformationScraperInput, InformationScraperOutput, PageScrapedData, \
+    Headings, Link, \
     Image
 from app.schemas.page_screenshotter import PageScreenshotterInput, PageScreenshotterOutput
+from app.schemas.state import State, StateBase, StateUpdate
 from app.schemas.website_coder import WebsiteCoderInput, WebsiteCoderOutput
