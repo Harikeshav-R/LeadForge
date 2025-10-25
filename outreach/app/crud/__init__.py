853d88534e222ab92de25f58728b1b3b293517e8
Harikeshav R <r.harikeshav@gmail.com>
2025-10-25 18:00:13 -0400

    feat(crud): add imports for Mail and State CRUD operations to module init
    - Add imports for CRUD functions of `Mail` and `State` to `outreach/app/crud/__init__.py`.
    - Centralize access to CRUD functionalities for better organization and modularity.

---
+++ outreach/app/crud/__init__.py
@@ -0,0 +1,2 @@
+from app.crud.mail import create_mail, read_mail, read_all_mails, update_mail, delete_mail
+from app.crud.state import create_state, read_state, read_all_states, update_state, delete_state
